0562d0286e41ea4a3c36d4bc896ad8ce602ef484
johnnyforde <john.forde@seas.upenn.edu>
2016-01-24 02:05:28 -0500

    Changes to listview and baseadapter.

--- app/src/main/java/com/example/johnny/pennapps/MainActivity.java
+++ app/src/main/java/com/example/johnny/pennapps/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.johnny.pennapps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends Activity {
@@ -9,7 +14,25 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.layout_primary);
+        setContentView(R.layout.activity_main);
+
+        ListView list = (ListView) findViewById(R.id.SCHEDULE);
+
+        ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("train", "101");
+        map.put("from", "6:30 AM");
+        map.put("to", "7:40 AM");
+        mylist.add(map);
+        map = new HashMap<String, String>();
+        map.put("train", "103(x)");
+        map.put("from", "6:35 AM");
+        map.put("to", "7:45 AM");
+        mylist.add(map);
+// ...
+        BaseAdapter in = new ListViewAdapter(this, mylist, R.layout.listview_row,
+                new String[] {"train", "from", "to"}, new int[] {R.id.FirstText, R.id.SecondText, R.id.ThirdText});
+        list.setAdapter();
     }
 }
 
